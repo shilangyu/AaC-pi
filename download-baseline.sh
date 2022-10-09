606557c91104871351b8bb20144b395979467820
shilangyu <xmarcinmarcin@gmail.com>
2022-10-09 12:01:46 +0200

    Initial commit

---
+++ download-baseline.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -o- https://stuff.mit.edu/afs/sipb/contrib/pi/pi-billion.txt | gzip > pi-baseline.txt.gz
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // prints first 800 digits of pi,
+  // source: https://cs.uwaterloo.ca/~alopez-o/math-faq/mathtext/node12.html
+  int a = 10000, b = 0, c = 2800, d, e, f[2801], g;
+  for (; b - c;)
+    f[b++] = a / 5;
+  for (; d = 0, (g = c * 2); c -= 14, printf("%.4d", e + d / a), e = d % a)
+    for (b = c; d += f[b] * a, f[b] = d % --g, d /= g--, --b; d *= b)
+      ;
+}

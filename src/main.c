12a3a954443468c7868ebceca2ab965b0a9b2bfb
Kamil Monicz <kamil@monicz.pl>
2022-10-17 22:33:13 +0200

    pr suggestions

--- src/main.c
+++ src/main.c
@@ -19,11 +19,11 @@ void compare_zero_rest(char *buff, size_t read) {
 //      -2: comparison error
 //  -1: equals
 // >-1: first diff index
-int compare(const char *file_name_1, const char *file_name_2) {
-  int result = -1;
+int64_t compare(const char *file_name_1, const char *file_name_2) {
+  int64_t result = -1;
 
-  char *buff_1 = (char *) malloc(COMPARE_BUFFER_SIZE);
-  char *buff_2 = (char *) malloc(COMPARE_BUFFER_SIZE);
+  char buff_1[COMPARE_BUFFER_SIZE];
+  char buff_2[COMPARE_BUFFER_SIZE];
 
   int64_t *buff_1_64 = (int64_t *) buff_1;
   int64_t *buff_2_64 = (int64_t *) buff_2;
@@ -31,23 +31,25 @@ int compare(const char *file_name_1, const char *file_name_2) {
   FILE *file_1 = fopen(file_name_1, "r");
   FILE *file_2 = fopen(file_name_2, "r");
 
-  while (result == -1) {
+  for (int64_t iter = 0; result == -1; iter++) {
     size_t read_1 = fread(buff_1, 1, COMPARE_BUFFER_SIZE, file_1);
     size_t read_2 = fread(buff_2, 1, COMPARE_BUFFER_SIZE, file_2);
 
     compare_zero_rest(buff_1, read_1);
     compare_zero_rest(buff_2, read_2);
 
-    for (int i = 0; i < COMPARE_BUFFER_SIZE / sizeof(int64_t); i++) {
+    for (int64_t i = 0; i < COMPARE_BUFFER_SIZE / sizeof(int64_t); i++) {
       if (buff_1_64[i] == buff_2_64[i])
         continue;
 
       // for error checking
       result = -2;
 
-      for (int j = i * sizeof(int64_t); j < (i + 1) * sizeof(int64_t); j++)
-        if (buff_1[j] != buff_2[j])
-          result = j;
+      for (int64_t j = i * sizeof(int64_t); j < (i + 1) * sizeof(int64_t); j++)
+        if (buff_1[j] != buff_2[j]) {
+          result = j + iter * COMPARE_BUFFER_SIZE;
+          break;
+        }
 
       break;
     }
@@ -60,15 +62,12 @@ int compare(const char *file_name_1, const char *file_name_2) {
   fclose(file_1);
   fclose(file_2);
 
-  free(buff_1);
-  free(buff_2);
-
   return result;
 }
 
 int main(const int argc, const char **argv) {
-  int result = compare("a.txt", "b.txt");
-  printf("\nCompare result: %i\n\n", result);
+  int64_t result = compare("a.txt", "b.txt");
+  printf("\nCompare result: %li\n\n", result);
 
   args_t args = parse_args(argv, argc);
 
